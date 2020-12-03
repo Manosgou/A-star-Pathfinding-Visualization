893f7f824544de70aca421fe09744ac9bbf65635
Manosgou <manolisgouvrikos@gmail.com>
2020-12-03 15:41:05 +0200

    Implement a better heuristic function(Manhattan distance) Find final path

--- src/AStar.java
+++ src/AStar.java
@@ -1,29 +1,42 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import static com.raylib.Colors.*;
 
 public class AStar {
 
     Cube [][] grid;
-    ArrayList<Cube> finalPath;
+     private ArrayList<Cube> finalPath;
 
     private ArrayList <Cube> openSet,closedSet;
+
+    private HashMap<Cube,Cube> cameFrom;
 
     public AStar(Cube [][]grid){
         this.grid =grid;
         this.openSet = new ArrayList<>();
         this.closedSet = new ArrayList<>();
         this.finalPath = new ArrayList<>();
+        this.cameFrom =  new HashMap<>();
+
     }
 
     private float heuristic(Cube neighbour,Cube end){
-        return Math.abs(neighbour.x() - end.x() + neighbour.y() - end.y());
+        return Math.abs(neighbour.x() - end.x()) + Math.abs(neighbour.y() - end.y());
 
     }
 
-
+    private void reconstructPath(Cube current){
+            while(this.cameFrom.containsKey(current)){
+                current = this.cameFrom.get(current);
+                this.finalPath.add(current);
+            }
+    }
 
     public boolean solveAStar(Cube start,Cube end){
             this.openSet.add(start);
+
+
             Cube current;
             int lowestF =0;
             while (!this.openSet.isEmpty()){
@@ -36,6 +49,10 @@ public class AStar {
                 }
                 current = this.openSet.get(lowestF);
                 if(current == end){
+                    this.reconstructPath(current);
+                    for(Cube cube : this.finalPath){
+                        cube.setColor(BLACK);
+                    }
                     return true;
                 }
 
@@ -54,19 +71,30 @@ public class AStar {
                             n.setGScore(tempG);
                             openSet.add(n);
                         }
+                        this.cameFrom.put(n,current);
                         n.setHScore(this.heuristic(n,end));
                         n.setFScore(n.getGScore()+n.getHScore());
+
 
                     }
 
                 }
-                for(Cube cube : openSet){
+
+
+
+
+                for(Cube cube : this.openSet){
                     cube.setColor(PURPLE);
                 }
-                for(Cube cube : closedSet){
+
+                for(Cube cube : this.closedSet){
                     cube.setColor(YELLOW);
                 }
+
+
+
             }
+
 
 
             return false;
